e234b9bd178325296946f2faadaf6ab05c53f818
Sean Liao <sean+git@liao.dev>
2022-06-06 13:07:21 +0100

    cloud logging output

--- go.mod
+++ go.mod
@@ -5,15 +5,15 @@ go 1.19
 require (
 	github.com/go-logr/logr v1.2.3
 	github.com/iand/logfmtr v0.2.1
-	google.golang.org/grpc v1.46.2
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93
+	google.golang.org/grpc v1.47.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.7 // indirect
-	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220527130721-00d5c0f3be58 // indirect
+	google.golang.org/genproto v0.0.0-20220602131408-e326c6e8e9c8 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- tools.go
+++ tools.go
@@ -3,10 +3,10 @@ package svcrunner
 import (
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/logr/funcr"
-	"github.com/iand/logfmtr"
 	"go.seankhliao.com/svcrunner/envflag"
 )
 
@@ -18,21 +18,53 @@ type Tools struct {
 
 func (t *Tools) register(c *envflag.Config) {
 	c.StringVar(&t.logfmt, "log.format", "json", "log output format: text|json")
-	c.IntVar(&t.verbosity, "log.verbosity", 0, "log verbosity: 0-2")
+	c.IntVar(&t.verbosity, "log.verbosity", 0, "log verbosity (error/info/debug/trace): -1 - 2")
 }
 
 func (t *Tools) init(out io.Writer) error {
 	switch t.logfmt {
 	case "text":
-		t.Log = logfmtr.NewWithOptions(logfmtr.Options{
-			Writer: out,
+		t.Log = funcr.New(func(prefix, args string) {
+			fmt.Fprintln(out, prefix, args)
+		}, funcr.Options{
+			LogTimestamp:    true,
+			TimestampFormat: time.RFC3339,
+			Verbosity:       t.verbosity,
 		})
-		logfmtr.SetVerbosity(t.verbosity)
 	case "json":
 		t.Log = funcr.NewJSON(func(obj string) {
 			fmt.Fprintln(out, obj)
 		}, funcr.Options{
 			Verbosity: t.verbosity,
+		})
+	case "json+gcp":
+		t.Log = funcr.NewJSON(func(obj string) {
+			fmt.Fprintln(out, obj)
+		}, funcr.Options{
+			Verbosity: t.verbosity,
+			RenderBuiltinsHook: func(kvList []any) []any {
+				out := make([]any, 0, len(kvList)+2)
+				out = append(out, "severity", "ERROR")
+				for i := 0; i < len(kvList); i += 2 {
+					switch kvList[i].(string) {
+					case "level":
+						switch kvList[i+1].(int) {
+						case 0:
+							out[1] = "INFO"
+						case 1, 2:
+							out[1] = "DEBUG"
+						default:
+							out[1] = "DEFAULT"
+						}
+					case "msg":
+						out = append(out, "message", kvList[i+1])
+					default:
+						out = append(out, kvList[i], kvList[i+1])
+					}
+				}
+
+				return out
+			},
 		})
 	default:
 		return fmt.Errorf("unknown log format: %v", t.logfmt)
