ca3cdc5fb5bba33535c8efe653567790e4e7e64a
Sean Liao <sean+git@liao.dev>
2022-06-08 17:23:28 +0100

    simplify gchat log

--- tools.go
+++ tools.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -60,14 +59,13 @@ func (t *Tools) init(out io.Writer) error {
 	case "json+gcp":
 		t.Log = funcr.NewJSON(func(obj string) {
 			fmt.Fprintln(out, obj)
+			if t.gchat != nil {
+				gchatReport(t.gchat, obj)
+			}
 		}, funcr.Options{
 			Verbosity: t.verbosity,
 			RenderBuiltinsHook: func(kvList []any) []any {
-				out := kvListToGCPLog(kvList)
-				if t.gchat != nil {
-					gchatReport(t.gchat, out)
-				}
-				return out
+				return kvListToGCPLog(kvList)
 			},
 		})
 	default:
@@ -76,23 +74,9 @@ func (t *Tools) init(out io.Writer) error {
 	return nil
 }
 
-func gchatReport(client *gchat.WebhookClient, kvList []any) {
-	var found bool
-	for i := 0; i < len(kvList); i += 2 {
-		if kvList[i].(string) == "error" {
-			found = true
-			break
-		}
-	}
-	if !found {
-		return
-	}
-	var buf strings.Builder
-	for i := 0; i < len(kvList); i += 2 {
-		fmt.Fprintf(&buf, "%s=%v ", kvList[i], kvList[i+1])
-	}
+func gchatReport(client *gchat.WebhookClient, obj string) {
 	client.Post(context.Background(), gchat.WebhookPayload{
-		Text: buf.String(),
+		Text: obj,
 	})
 }
 
@@ -100,6 +84,7 @@ func kvListToGCPLog(kvList []any) []any {
 	out := make([]any, 0, len(kvList)+2)
 	out = append(out, "severity", "ERROR")
 	for i := 0; i < len(kvList); i += 2 {
+		// note: RenderBuiltinsHook only works for logr/funcr builtin key/values
 		switch kvList[i].(string) {
 		case "level":
 			switch kvList[i+1].(int) {
